e493b9018a400be89e78a470acdbd556d2057c58
Remin Maksim <reminmax@gmail.com>
2022-02-22 00:33:18 +0300

    Added page navigation tracking example

--- MsAppCenter/MsAppCenter/PageModels/MainPageModel.cs
+++ MsAppCenter/MsAppCenter/PageModels/MainPageModel.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using System.Text;
 using FreshMvvm;
+using MsAppCenter.Services.AnalyticsService;
 
 namespace MsAppCenter.PageModels
 {
     class MainPageModel : FreshBasePageModel
     {
-        public MainPageModel()
+        protected new IAnalyticsService AnalyticsService { get; private set; }
+
+        public MainPageModel(IAnalyticsService analyticsService)
         {
-            
+            // Use dependency injection
+            AnalyticsService = analyticsService;
         }
+
+        protected override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
+
+            // Track event example
+            AnalyticsService.TrackEvent("Navigated to the MainPage",
+                new Dictionary<string, string>
+                {
+                    { "Date", DateTime.Now.ToString("dd/MM/yyyy") },
+                });
+
+        }
+
     }
 }
